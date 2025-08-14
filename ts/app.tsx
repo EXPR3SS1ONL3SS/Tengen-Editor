77906e414d0b876a323c9c11b224c6471748e62e
Master Tengen 💯 <219903610+EXPR3SS1ONL3SS@users.noreply.github.com>
2025-08-14 12:50:48 +0000

    s

--- ts/app.tsx
+++ ts/app.tsx
@@ -1,16 +1,18 @@
 import * as react from 'react'; import * as rDom from 'react-dom';
-import { createRoot } from 'react-dom/client';
+import * as client from 'react-dom/client'
 
-function App() {
-  return (
-    <div style={{width:'30%', height:'30%'}}>
-      text
-    </div>
-  )
+class App extends react.Component {
+  render() {
+      return (
+      <div style={{width:'30%', height:'30%', color:'black'}}>
+        text
+      </div>
+      );
+  }
 }
 
 const container = document.getElementById('tengen-editor-root');
 if (container) {
-    const root = createRoot(container);
+    const root = client.hydrateRoot(container, <App/>);
     root.render(<App/>);
 }
